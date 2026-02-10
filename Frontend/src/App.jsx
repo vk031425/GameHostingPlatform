22a54220e91551b73e3a0b9e3d4c27000fc37b45
Vinay_ <vinay989633@gmail.com>
2026-02-10 15:29:23 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import GamePage from "./GamePage";
+
+const App = ()=>{
+  return(
+    <GamePage/>
+  )
+}
+
+export default App;
---
+++ Frontend/src/GamePage.jsx
@@ -0,0 +1,15 @@
+const GamePage = () => {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://pub-7f6e627580c7494f988dcdcb8f8d1af9.r2.dev/sample/index.html"
+        width="100%"
+        height="100%"
+        style={{ border: "none" }}
+        allow="fullscreen"
+      />
+    </div>
+  );
+};
+
+export default GamePage;
